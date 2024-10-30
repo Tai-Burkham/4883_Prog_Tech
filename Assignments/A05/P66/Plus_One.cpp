43d3228a8a23bbf3dfe39b588b7f058f0d56a119
Tai-Burkham <68305024+Tai-Burkham@users.noreply.github.com>
2024-10-29 20:27:17 -0500

    Create Plus_One.cpp

---
+++ Assignments/A05/P66/Plus_One.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size() - 1;
+        
+        for (int i = n; i >= 0; i--) 
+        {
+            if (digits[i] < 9) 
+            {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
