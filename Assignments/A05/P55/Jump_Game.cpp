662c3596ef6133b00df35f7388a09506ee67406e
Tai-Burkham <68305024+Tai-Burkham@users.noreply.github.com>
2024-11-05 10:08:59 -0600

    Update Jump_Game.cpp

--- Assignments/A05/P55/Jump_Game.cpp
+++ Assignments/A05/P55/Jump_Game.cpp
@@ -1,1 +1,24 @@
-// Code here
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int jump = 0;
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            if (i > jump) 
+            {
+                return false;
+            }
+
+            if (i + nums[i] > jump) 
+            {
+                jump = i + nums[i];
+            }
+        }
+        if (jump >= nums.size() - 1) 
+        {
+            return true; 
+        } else {
+            return false;
+        }
+    }
+};
